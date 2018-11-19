b71646ae03ae98b36b1ce0e107dbfab180b5cc7d
kougei-kyabehyon <38421015+kougei-kyabehyon@users.noreply.github.com>
2018-11-19 16:57:12 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,4 +1,14 @@
-window.alert('こんばんは');
-console.log('こんにちは');
-var result= window.confirm('準備');
-document.getElementById('choice').textContent= new Date(); 
+
+var number= Math.floor(Math.random()*3);
+var answer=parseInt(window.prompt('かずあて'));
+var massage;
+if(answer==nunber)
+{
+massage='good';
+}
+else if(answer<number){
+massage='もうちょいうえ';
+}else if(answer>number){
+massage='もうちょいした';
+}
+document.getElementById('choice').textContent= message; 
